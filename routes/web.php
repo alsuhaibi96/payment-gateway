8c32e76939f22abd102c054067253dc7fac76577
AhdKarman <karahdman1@gmail.com>
2022-04-21 16:20:52 +0300

    linking pages

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('website/index');
 });
 Route::get('/yemenpay', function () {
     return view('website/index');
 });
+
+Route::get('/login', function () {
+    return view('website/login');
+}) -> name('login');
+
+Route::get('/register', function () {
+    return view('website/register');
+}) -> name('register');

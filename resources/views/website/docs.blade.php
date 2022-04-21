180a0a8808303f16bd9d2184aebe7999d9c0ea18
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-04-21 17:48:10 +0300

    edit

--- resources/views/website/docs.blade.php
+++ resources/views/website/docs.blade.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wasel pay</title>
-   
+        <!--font icon-->
+    <link rel="stylesheet" href="{{url('assets/web/path/to/font-awesome/css/font-awesome.min.css')}}">
+   <!--custom css file link-->
     
    
     <link rel="stylesheet" href="{{url('assets/web/css/doc.css')}}">

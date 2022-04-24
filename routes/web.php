bea78c7e3881f112f2a21eacefbdd7dfd327a077
Alsuhaibi <alsuhaibiabdulrahman@gmail.com>
2022-04-24 21:15:55 +0300

    roles_permissions

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Settings\SettingController;
+use App\Http\Controllers\user\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * Register and sign in routes 
+ */
+
+ Route::get('/user/register',[UserController::class,'viewRegisteration'])->name('registeration');
+ Route::get('/customer/register',[UserController::class,'viewCustomerRegister'])->name('customer_register');
+ Route::get('/merchant/register',[UserController::class,'viewMerchantRegister'])->name('merchant_register');
+ Route::post('/Signin',[UserController::class,'viewRegisterationPage'])->name('Signin');
+ 
+
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/yemenpay', function () {
     return view('website/index');
 });
+
+
+
+/**
+ * Adding roles for the authentication and users
+ * You should go to this route every time the database data are lost
+ */
+Route::get('/generate_roles',[SettingController::class,'generateRoles']);
